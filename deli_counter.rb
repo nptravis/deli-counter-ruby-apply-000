3edd6a3ef5383cad6645aaefe03b471dfe190249
Nic Travis <nptravis@yahoo.com>
2018-01-29 09:10:18 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 katz_deli = []
 def take_a_number(line, name)
   line.push(name)
-  return "Welcome, #{name}. Your are number #{line.length} in line"
+  puts "Welcome, #{name}. Your are number #{line.length} in line"
 end
 
 def now_serving(line)
